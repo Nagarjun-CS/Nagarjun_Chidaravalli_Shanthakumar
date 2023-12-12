f9d87ff7d712595a7248016162b12c6a49f76799
Nagarjun-CS <nagarjuncs007@gmail.com>
2023-12-11 23:49:00 -0800

    Bug and UI fix

--- Nagarjun_Chidaravalli_Shanthakumar/confirm/views.py
+++ Nagarjun_Chidaravalli_Shanthakumar/confirm/views.py
@@ -14,11 +14,8 @@ def confirm(request):
 def deleteRecord(request):
     deleteName = request.POST.get("name")
     print("---------------------------",deleteName)
-    #timestamp_str = "2023-12-03 05:09:48.153312"
-
-    #timestamp_dt = datetime.strptime(timestamp_str, "%Y-%m-%d %H:%M:%S.%f")
-
     c = Create.objects.get(name=deleteName)
+    print("**************************",c)
     c.delete()
     allData = Create.objects.all()
     return render(request, 'home.html', {'allData':allData})
